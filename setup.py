5f939f41da064c0c82b475bd6bd84421a662dea3
iamravitejaDevOps <sureraviteja23@gmail.com>
2026-02-03 19:21:22 +0530

    Intial template.py,MongoDB,Logging,Exception and Utils added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+ # it is related to __init__.py each folder for packages
+setup(
+  name="us-visa",
+  version="0.0.0",
+  author="Ravi Teja",
+  author_email="sureraviteja23@gmail.com",
+  packages=find_packages()
+)
